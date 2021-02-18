463e4d4197d2b1be5debb1f0d0499060031aabf1
andres2020martinez <andres.martinez@unidech.mx>
2021-02-17 20:16:39 -0600

    final, por verificar

--- server.py
+++ server.py
@@ -1,92 +1,52 @@
-'''from flask import Flask
-
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def home_page():
-    return "My movie collection"
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)'''
-
-'''from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def home_page():
-    return render_template("home.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)'''
-
-'''from datetime import datetime
-
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def home_page():
-    today = datetime.today()
-    day_name = today.strftime("%A")
-    return render_template("home.html", day=day_name)
-
-@app.route("/movies")
-def movies_page():
-    return render_template("movies.html")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)'''
-
-'''from flask import Flask
-
-import views
-
-
-def create_app():
-    app = Flask(__name__)
-
-    app.add_url_rule("/", view_func=views.home_page)
-    app.add_url_rule("/movies", view_func=views.movies_page)
-
-    return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host="0.0.0.0", port=8080, debug=True)'''
-
+#normaluser normaluserpw
+#admin  adminpw
 from flask import Flask
+from flask_login import LoginManager
 
 import views
 from database import Database
 from movie import Movie
+from user import get_user
+import os
 
+lm = LoginManager()
+
+@lm.user_loader
+def load_user(user_id):
+    return get_user(user_id)
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object("settings")
 
     app.add_url_rule("/", view_func=views.home_page)
-    #app.add_url_rule("/movies", view_func=views.movies_page)
-    app.add_url_rule("/movies", view_func=views.movies_page, methods=["GET", "POST"])
-    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
-    app.add_url_rule("/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"])
 
-    db = Database()
-    db.add_movie(Movie("Slaughterhouse-Five", year=1972))
-    db.add_movie(Movie("The Shining"))
+    app.add_url_rule(
+        "/login", view_func=views.login_page, methods=["GET", "POST"]
+    )
+    app.add_url_rule("/logout", view_func=views.logout_page)
+
+    app.add_url_rule(
+        "/movies", view_func=views.movies_page, methods=["GET", "POST"]
+    )
+    app.add_url_rule("/movies/<int:movie_key>", view_func=views.movie_page)
+    app.add_url_rule(
+        "/movies/<int:movie_key>/edit",
+        view_func=views.movie_edit_page,
+        methods=["GET", "POST"],
+    )
+    app.add_url_rule(
+        "/new-movie", view_func=views.movie_add_page, methods=["GET", "POST"]
+    )
+
+    lm.init_app(app)
+    lm.login_view = "login_page"
+
+    home_dir = os.path.expanduser("~")
+    db = Database(os.path.join(home_dir, "movies.sqlite"))
     app.config["db"] = db
+    #db = Database()
+    #app.config["db"] = db
 
     return app
 
